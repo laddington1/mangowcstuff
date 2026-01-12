9e73c84fe5352010e412b96d9d0985cfc1b0bb5c
laddington1 <laddington6767@gmail.com>
2026-01-12 21:39:55 +0800

    dots from Mango

---
+++ autostart.sh
@@ -0,0 +1,3 @@
+/usr/bin/swaybg -i /home/kant/Downloads/MacOS1.jpg >/dev/null 2>&1 &
+/usr/bin/waybar -c /home/kant/.config/mango/config.jsonc -s /home/kant/.config/mango/style.css >/dev/null 2>&1 &
+elephant &
